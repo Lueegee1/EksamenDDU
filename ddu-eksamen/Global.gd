f3f809f6c4276db68ea4953789efb9b541b8a4d6
Fredeblok0912 <frederikblokhellesoe@gmail.com>
2026-02-23 11:06:32 +0100

    Created folders and added more comments to global

--- ddu-eksamen/Global.gd
+++ ddu-eksamen/Global.gd
@@ -1,12 +1,11 @@
 extends Node2D
+#class prototype for colonist i guess
 
 
-# Called when the node enters the scene tree for the first time.
 #func _ready() -> void:
 #	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta: float) -> void:
 #	pass
 
@@ -15,4 +14,5 @@ extends Node2D
 	#Spawn new colonist
 	#Ressources
 	#SPawn events
+	#
 	
