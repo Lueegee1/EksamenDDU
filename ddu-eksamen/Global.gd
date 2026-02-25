b532ccf77b270aa5a4017235a96806461f6abdf7
Fredeblok0912 <frederikblokhellesoe@gmail.com>
2026-02-25 13:04:15 +0100

    added tick function

---
+++ ddu-eksamen/Global.gd
@@ -0,0 +1,29 @@
+extends Node2D
+#class prototype for colonist i guess
+var research_points: int = 0
+var food: int = 0
+var plant_matter: int = 0
+var minerals: int = 0
+
+var timer: float = 0
+var tick_interval: float = 1.0
+
+#func _ready() -> void:
+#	pass # Replace with function body.
+
+func tick():
+	emit_signal("tick_signal")
+	pass
+	#Food
+	#Spawn new colonist
+	#Ressources
+	#Spawn events
+
+func _process(delta: float) -> void:
+	timer += delta
+	if timer > tick_interval:
+		tick()
+
+
+
+	
