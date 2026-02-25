aa9e2fcebc075077dd9fae623f35da37b35222cc
CoinLords <eliot.rc@outlook.fr>
2026-02-25 21:33:31 +0100

    added traits to json file, expanded comment documentation

--- ddu-eksamen/Scripts/GameManager.gd
+++ ddu-eksamen/Scripts/GameManager.gd
@@ -1,21 +1,15 @@
 extends Node
 
-var colony_start_amount = 6
-var colonist_dict: Dictionary = {}
-var assignment_dict: Dictionary = {}
-var name_array: Array = []
-var trait_dict = {
-	000:{"name":"Neutral","productivity_mod":1,"happiness_mod":1,"sickness_chance":1},
-	001:{"name":"Sanguine","productivity_mod":1,"happiness_mod":1.1,"sickness_chance":1},
-	002:{"name":"Pessimistic","productivity_mod":1,"happiness_mod":0.9,"sickness_chance":1},
-	003:{"name":"Effective","productivity_mod":1.2,"happiness_mod":1,"sickness_chance":1},
-	004:{"name":"Lazy","productivity_mod":0.8,"happiness_mod":1,"sickness_chance":1},
-	005:{"name":"Healthy","productivity_mod":1,"happiness_mod":1,"sickness_chance":0.5},
-	006:{"name":"Sickly","productivity_mod":1,"happiness_mod":1,"sickness_chance":2},
-}
-var genetics_array = trait_dict.keys()
+# Setting Up Dictionaries, Lists and Variables -----------------------------------------------------------------------
 
-var current_tick = 0
+var colonist_dict: Dictionary = {}         
+var assignment_dict: Dictionary = {}
+var trait_dict: Dictionary = {}
+var name_array: Array = []
+
+var colony_start_amount = 6                # the amount of colonists that starts in the colony
+var total_trait_amount = 6                 # the amount of traits each colonist has
+var current_tick = 0                       # sets current_tick to zero
 
 # Loading Data from JSON Files ----------------------------------------------------------------------------------
 
@@ -27,11 +21,21 @@ func load_names_from_json(path: String) -> void:
 	var _result = json.parse(json_text)
 	name_array = json.data["names"]
 
+func load_traits_from_json(path: String) -> void:
+	var file = FileAccess.open(path, FileAccess.READ)
+	var json_text = file.get_as_text()
+	file.close()
+	var json = JSON.new()
+	var _result = json.parse(json_text)
+	var temp_dict = json.data["traits"]
+	for key in temp_dict.keys():
+		trait_dict[int(key)] = temp_dict[key]
+
 # Game Setup ----------------------------------------------------------------------------------
 
 func generate_colonist_name() -> String:
 	if name_array.is_empty():
-		return "ERROR#NANINF/DIV0"
+		return "ERROR#NANINF/DIV0" # good luck getting this name lol
 	var generated_name = name_array.pick_random()
 # rerolls name if duplicate
 	while colonist_dict.has(generated_name):
@@ -40,8 +44,8 @@ func generate_colonist_name() -> String:
 
 func generate_starter_trait_array():
 	var temp_trait_array = []
-	var duplicate_genetic_array = genetics_array.duplicate()
-	for number in 6:
+	var duplicate_genetic_array = trait_dict.keys()
+	for number in total_trait_amount:
 		var random = duplicate_genetic_array.pick_random()
 		temp_trait_array.append(random)
 		duplicate_genetic_array.erase(random)
@@ -64,6 +68,7 @@ func get_new_colony(colony_population):
 
 func _ready():
 	load_names_from_json("res://data/names.json")
+	load_traits_from_json("res://data/traits.json")
 	get_new_colony(colony_start_amount)
 
 # Tick System --------------------------------------------------------------------------------------------------
