6349b0b6103dc1a8194f430daf765f2f35e44c78
CoinLords <eliot.rc@outlook.fr>
2026-02-25 14:08:15 +0100

    mage GameManager.gd

---
+++ ddu-eksamen/Scripts/GameManager.gd
@@ -0,0 +1,10 @@
+extends Node
+
+var colonist_amount 
+var colonist_dict
+
+func add_colonist(name,trait_1,trait_2,trait_3,trait_4,trait_5,trait_6):
+	colonist_dict.append({
+	"id":001,"name":name,trait 
+})
+	pass
