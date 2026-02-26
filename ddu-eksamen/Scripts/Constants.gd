ec0aff61bc69ca564df9cb60282482eba9d43ddc
CoinLords <eliot.rc@outlook.fr>
2026-02-26 10:48:58 +0100

    fixed bugs and added more traits

--- ddu-eksamen/Scripts/Constants.gd
+++ ddu-eksamen/Scripts/Constants.gd
@@ -3,7 +3,7 @@ extends Node
 func _ready():
 	# Remember to set as global
 	Global.Constants = self
-
+	pass
 
 # World
 const GRAVITY := 6500.0
--- ddu-eksamen/Scripts/GameManager.gd
+++ ddu-eksamen/Scripts/GameManager.gd
@@ -47,10 +47,10 @@ func generate_starter_trait_array():
 	var duplicate_genetic_array = trait_dict.keys()
 	for number in total_trait_amount:
 		var random = duplicate_genetic_array.pick_random()
-		if random != 000:
+		if random != 0:
 			temp_trait_array.append(random)
 			duplicate_genetic_array.erase(random)
-		elif random == 000:
+		elif random == 0:
 			temp_trait_array.append(random)
 	return temp_trait_array
 
