6a420ec0e15b07d787eab17035a1ffe79da20470
Lueegee1 <86802628+Lueegee1@users.noreply.github.com>
2026-03-16 09:18:02 +0100

    Screen is HD now

--- ddu-eksamen/Scenes/UI.gd
+++ ddu-eksamen/Scenes/UI.gd
@@ -2,18 +2,21 @@ extends CanvasLayer
 
 @onready var happiness_bar = $"HappinessBar/HappinessBarColorRect"
 
-var bar_length = 500
-var screen_dim= Vector2(1152,648)
+var bar_length = 200
+var screen_dim= Vector2(1920,1080)
 func position_elements():
 	#Happiness bar
 	happiness_bar.position = Vector2(((screen_dim.x-bar_length)/2),50)
-	happiness_bar.size=Vector2(bar_length,50)
+	happiness_bar.size=Vector2(bar_length,30)
+	happiness_bar.color = Color(0.231, 1.0, 0.302, 1.0)
 
 func _ready() -> void:
 	position_elements()
-	pass # Replace with function body.
+
+func update_happiness_bar():
+	bar_length = (Global.average_happiness/100)*(2*screen_dim.x/5)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	delta = delta
+	update_happiness_bar()
